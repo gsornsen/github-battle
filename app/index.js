fd18c074667f58866dadced843b24708d79f2354
gsornsen <gsornsen@gmail.com>
2019-03-11 19:17:59 -0700

    Adding PropTypes

--- app/index.js
+++ app/index.js
@@ -1,18 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css')
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello Gerald!
+        Hello {this.props.name}!
       </div>
     )
   }
 }
 
+App.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
 ReactDOM.render(
-  <App />,
+  <App 
+    name="Gerald"
+  />,
   document.getElementById('app')
 )
